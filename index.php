e21ae2156077036f1ac2f2e8342ea7ed93080cc2
Marek Błaszczyk <mareczekblaszczyk@gmail.com>
2024-09-27 10:09:23 +0200

    dodano 5 wierszy

--- index.php
+++ index.php
@@ -13,6 +13,11 @@
         <p>Ile rogali(0.99PLN/szt):<input  type="text" name="rogale" required></p>
         <p>Ile bułek(1.29PLN/szt):<input  type="text" name="bulka"  required></p>
         <p>Ile soków(2.45PLN/szt):<input  type="text" name="sokow"  required></p>
+        <p>Ile chipsy(7.00PLN/szt):<input  type="text" name="chipsy"  required></p>
+        <p>Ile mleko(2.99PLN/szt):<input  type="text" name="mleko"  required></p>
+        <p>Ile czekolada(5.25PLN/szt):<input  type="text" name="czekolada"  required></p>
+        <p>Ile masło(1.45PLN/szt):<input  type="text" name="maslo"  required></p>
+        <p>Ile wodka(20.00PLN/szt):<input  type="text" name="wodka"  required></p>
         <input type="Submit" value="Wyslij zamówienie">
         <input type="reset"value="Wycyść">
     </form>
--- order.php
+++ order.php
@@ -11,11 +11,21 @@
 $rogale=$_POST['rogale'];
 $bulka=$_POST['bulka'];
 $sokow=$_POST['sokow'];
-$suma=(0.99*$rogale)+(1.29*$bulka)+(2.45*$sokow);
-$sum=($rogale)+($bulka)+($sokow);
+$chipsy=$_POST['chipsy'];
+$mleko=$_POST['mleko'];
+$czekolada=$_POST['czekolada'];
+$maslo=$_POST['maslo'];
+$wodka=$_POST['wodka'];
+$suma=(0.99*$rogale)+(1.29*$bulka)+(2.45*$sokow)+(7.00*$chipsy)+(2.99*$mleko)+(5.25*$czekolada)+(1.45*$maslo)+(20.00*$wodka);
+$sum=($rogale)+($bulka)+($sokow)+($chipsy)+($mleko)+($czekolada)+($maslo)+($wodka);
 $srogale=(0.99*$rogale);
-$sbulka=(1.29*$rogale);
-$ssokow=(2.45*$rogale);
+$sbulka=(1.29*$bulka);
+$ssokow=(2.45*$sokow);
+$schipsy=(7.00*$chipsy);
+$smleko=(2.99*$mleko);
+$sczekolada=(5.25*$czekolada);
+$smaslo=(1.45*$maslo);
+$swodka=(20.00*$wodka);
 echo<<<END
 <th2>Podsumowanie zamówienia</h2>
 <table border="1" callpadding="10" callspacing="0" bgcolor="darkgreen">
@@ -30,6 +40,21 @@ echo<<<END
 </tr>
 <tr  bgcolor="lightgrey">
 <td> sokow (2.45PLN/szt)</td><td>$sokow </td><td>$ssokow PLN</td>
+</tr>
+<tr  bgcolor="lightgrey">
+<td> chipsy (7.00PLN/szt)</td><td>$chipsy </td><td>$schipsy PLN</td>
+</tr>
+<tr  bgcolor="lightgrey">
+<td> mleko (2.99PLN/szt)</td><td>$mleko </td><td>$smleko PLN</td>
+</tr>
+<tr  bgcolor="lightgrey">
+<td> czekolada (5.25PLN/szt)</td><td>$czekolada</td><td>$sczekolada PLN</td>
+</tr>
+<tr  bgcolor="lightgrey">
+<td> masło (1.45PLN/szt)</td><td>$maslo </td><td>$smaslo PLN</td>
+</tr>
+<tr  bgcolor="lightgrey">
+<td> wodka (20.00PLN/szt)</td><td>$wodka </td><td>$swodka PLN</td>
 </tr>
 <tr bgcolor="darkgrey">
 <td> SUMA</td><td>$sum </td><td>$suma PLN</td>
